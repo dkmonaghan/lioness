308e738b9d368a3f68efcd513fc68e8dd50a8257
Daniel Monaghan <GLaDOSDan@users.noreply.github.com>
2016-12-08 11:37:16 +0000

    - Changed repsonses so that everything is a JSON response

--- lioness.py
+++ lioness.py
@@ -1,5 +1,4 @@
-# Author GLaDOSDan <gladosdan@gmail.com>
-
+# Author Daniel K Monaghan <dkm5@aber.ac.uk>
 import socket
 import sys
 import time
@@ -7,7 +6,7 @@ import json
 
 # Confirm we have enough arguments to proceed
 if len(sys.argv) < 3:
-	print "ARGV_ERROR"
+	print json.dumps({"RESULT": "ERROR", "STRING": "ARGV_ERROR"})
 	sys.exit()
 
 # Open connection to Extron IPL
@@ -18,7 +17,7 @@ try:
 
 # Catch any connection errors
 except socket.error:
-	print "CONNECT_ERROR"
+	print json.dumps({"RESULT": "ERROR", "STRING": "CONNECT_ERROR"})
 	sys.exit()
 
 # Execute commands
@@ -36,8 +35,8 @@ try:
 
 # Catch any read errors
 except socket.error:
-	print "READ_ERROR"
+	print json.dumps({"RESULT": "ERROR", "STRING": "READ_ERROR"})
 	sys.exit()
 
 # Format and print final response
-print json.dumps(response[3::])
+print json.dumps({"RESULT": "SUCCESS", "VALUES": response[3::]})
